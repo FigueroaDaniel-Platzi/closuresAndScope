1e5e2025696ac00ad4314118bc65bf144cab2172
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-26 15:52:44 -0500

    Practicando Closures  class9

---
+++ src/closure/myMoneyBox.js
@@ -0,0 +1,28 @@
+// Without closure function don't keep variables values
+// function moneyBox(coins) {
+//   let saveCoins = 0;
+//   saveCoins += coins;
+//   console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// moneyBox(5);
+// moneyBox(5);
+
+function moneyBox() {
+  let saveCoins = 0;
+  function countCoins(coins) {
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`)
+  }
+  return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
+
+const moneyBoxAnna = moneyBox();
+moneyBoxAnna(10);
+moneyBoxAnna(20);
+moneyBoxAnna(5);
